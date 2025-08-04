795347074347bbcf57edd875d27530c71fe36b30
Alexandre <sdolaqidze@gmail.com>
2025-08-04 21:00:20 +0400

    dsu.cpp

---
+++ Data_Structures/dsu.cpp
@@ -0,0 +1,46 @@
+#include<vector>
+#include<algorithm>
+#include<numeric>
+
+using namespace std;
+
+struct DSU {
+    vector<int> f, siz;
+    
+    DSU() {}
+    DSU(int n) {
+        init(n);
+    }
+    
+    void init(int n) {
+        f.resize(n);
+        iota(f.begin(), f.end(), 0);
+        siz.assign(n, 1);
+    }
+    
+    int leader(int x) {
+        while (x != f[x]) {
+            x = f[x] = f[f[x]];
+        }
+        return x;
+    }
+    
+    bool same(int x, int y) {
+        return leader(x) == leader(y);
+    }
+    
+    bool merge(int x, int y) {
+        x = leader(x);
+        y = leader(y);
+        if (x == y) {
+            return false;
+        }
+        siz[x] += siz[y];
+        f[y] = x;
+        return true;
+    }
+    
+    int size(int x) {
+        return siz[leader(x)];
+    }
+};

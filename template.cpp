89cf64259c5c73ba3a29734f0fffccfb0e200e09
Alexandre <sdolaqidze@gmail.com>
2025-12-15 01:25:49 +0400

    ADD code template

--- template.cpp
+++ template.cpp
@@ -26,17 +26,19 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 #define sz(v) (ll)v.size()
 #define pb push_back
+#define eb emplace_back
 #define bpc __builtin_popcount
 #define bpcll __builtin_popcountll
 #define S second
 #define F first
 #define vec vector
 #define show(x) cout<<#x<<"="<<x
+#define double double
 
-#define rep(i,a,b) for(int i = (a); i <= (b); i++)
-#define per(i,a,b) for(int i = (a); i >= (b); i--)
-#define fr(i,n) for(int i = 0; i < n; i++)
-#define rf(i,n) for(int i = n-1; i>=0; i--)
+#define rep(i,a,b) for(ll i = (a); i <= (b); i++)
+#define per(i,a,b) for(ll i = (a); i >= (b); i--)
+#define fr(i,n) for(ll i = 0; i < n; i++)
+#define rf(i,n) for(ll i = n-1; i>=0; i--)
 #define prend(x) {cout<<(x)<<"\n"; return;}
 
 #define PR cout<<
@@ -49,34 +51,29 @@ mt19937_64 gen(chrono::steady_clock::now().time_since_epoch().count());
 uniform_int_distribution<ll> rnd(0,LLONG_MAX);
 // ll randint = rnd(gen);
 
-int inf = INT_MAX;
-ll linf = LLONG_MAX;
+int inf = 1e9;
+ll linf = 1e18;
+const ll N = 200000;
 const ll M = 998244353;
-const ll N = 2e3;
 double pi = acos(-1);    
+bool multiCase = 0;
+const double eps = 1e-6;
 
+void solve(){}
 
-bool multiCase = 1;
-
-void solve() {
-}   
-
-
-
-void preprocess(){
-}
-
+void preprocess(){}
 
 int main() {
-#ifndef ONLINE_JUDGE
-    // freopen("in.txt", "r", stdin);
-    // freopen("out.txt", "w", stdout);
+#ifdef LOCAL
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
 #endif  
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    preprocess();
+    cout<<fixed<<setprecision(15);
     ll tt = 1;
     if(multiCase) cin >> tt;
+    preprocess();
     while (tt--){
         solve();
     }
